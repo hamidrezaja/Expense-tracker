7a90a5088c93e3f3ad26a0174adeff3221ca9784
hamidrezaja <hrjalili98@gmail.com>
2025-02-01 17:26:16 +0330

    Add logic to index view for daily, weekly, and monthly sum; update index template

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import ExpenseForm
 from .models import Expense
 from django.db.models import Sum
+import datetime
 # Create your views here.
 def index(request):
     if request.method=='POST':
@@ -10,8 +11,25 @@ def index(request):
             expense.save()
     expenses=Expense.objects.all()
     total_expenses=expenses.aggregate(Sum('amount'))
+    #logic for sum of last year expenses
+    last_year=datetime.date.today()-datetime.timedelta(days=365)
+    data=Expense.objects.filter(date__gt=last_year)
+    yearly_sum=data.aggregate(Sum('amount'))
+    #logic for sum of last month expenses
+    last_month=datetime.date.today()-datetime.timedelta(days=30)
+    data=Expense.objects.filter(date__gt=last_month)
+    monthly_sum=data.aggregate(Sum('amount'))
+    #logic for sum of last week expenses
+    last_week=datetime.date.today()-datetime.timedelta(days=7)
+    data=Expense.objects.filter(date__gt=last_week)
+    weekly_sum=data.aggregate(Sum('amount'))
+    daily_sums=Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    print(daily_sums)
     expense_form=ExpenseForm()
-    return render(request,'myapp/index.html',{"expense_form":expense_form,"expenses":expenses,'total_expenses':total_expenses})
+    return render(request,'myapp/index.html',{"expense_form":expense_form,"expenses":expenses,
+                                              'total_expenses':total_expenses,'yearly_sum':yearly_sum,
+                                              'monthly_sum':monthly_sum,'weekly_sum':weekly_sum,
+                                              'daily_sums':daily_sums})
 def edit(request,id):
     expense=Expense.objects.get(id=id)
     expense_form=ExpenseForm(instance=expense)
