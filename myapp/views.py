1a4e9f0f4d0e352052d8af1f84318a82ea2b1ace
hamidrezaja <hrjalili98@gmail.com>
2025-02-01 17:35:44 +0330

    Add categorical sum calculation and update index.html

--- myapp/views.py
+++ myapp/views.py
@@ -24,12 +24,12 @@ def index(request):
     data=Expense.objects.filter(date__gt=last_week)
     weekly_sum=data.aggregate(Sum('amount'))
     daily_sums=Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
-    print(daily_sums)
+    categorical_sums=Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
     expense_form=ExpenseForm()
     return render(request,'myapp/index.html',{"expense_form":expense_form,"expenses":expenses,
                                               'total_expenses':total_expenses,'yearly_sum':yearly_sum,
                                               'monthly_sum':monthly_sum,'weekly_sum':weekly_sum,
-                                              'daily_sums':daily_sums})
+                                              'daily_sums':daily_sums,'categorical_sums':categorical_sums})
 def edit(request,id):
     expense=Expense.objects.get(id=id)
     expense_form=ExpenseForm(instance=expense)
